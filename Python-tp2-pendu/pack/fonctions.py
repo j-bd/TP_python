25d835251d7f35d5c9b38f82b6de0809ccebad08
j-bd <recodata.out@gmail.com>
2018-10-31 16:44:39 +0100

    Fonction display deleted

--- Python-tp2-pendu/pack/fonctions.py
+++ Python-tp2-pendu/pack/fonctions.py
@@ -9,9 +9,9 @@ import pickle
 def scores_checking():
     """Checking and returning if an history of scores is already existing or create it"""
     try:
-        players_history = open("package/scores", "rb")
+        players_history = open("pack/scores", "rb")
     except FileNotFoundError:
-        players_history = open("package/scores", "wb")
+        players_history = open("pack/scores", "wb")
         scores_dict = dict()
         save = pickle.Pickler(players_history)
         save.dump(scores_dict)
@@ -33,25 +33,16 @@ def gamer_history(scores_dict):
         return answer, 0, scores_dict
 
 
-def gamer_letter():
+def gamer_letter(letter="0"):
     """Asking  the gamer to choose a letter and return it"""
-    letter = "0"
     while letter.isalpha() != True or len(letter) > 1:
         print("S'il vous plait, choisissez votre lettre: ")
         letter = input()
     return letter.lower()
 
-def display_word(gamer_word):
-    """Displaying the word to guess with '*' and 'letters' found"""
-    display = str()
-    for l in gamer_word:
-        display += l
-    print(display)
 
-
-def replay():
+def replay(answer="0"):
     """Offer the possibility to play again"""
-    answer = "0"
     while answer.lower() != "o" and answer.lower() != "n":
         print("Souhaitez-vous rejouer (o/n) ?")
         answer = input()
