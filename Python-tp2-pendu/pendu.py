1e03ab369a24940a50ec7539f5edd0271355d66c
j-bd <recodata.out@gmail.com>
2018-10-31 16:43:01 +0100

    Display elements improved

--- Python-tp2-pendu/pendu.py
+++ Python-tp2-pendu/pendu.py
@@ -15,18 +15,18 @@ scores_dict = pack.fonctions.scores_checking()
 player, score, scores_dict = pack.fonctions.gamer_history(scores_dict)
 attempt = 0
 
-with open("package/scores", "wb") as scores_file:
+with open("pack/scores", "wb") as scores_file:
 
     word_to_guess = list(random.choice(pack.donnees.WORDS))
-    gamer_answer = ["*" for x in word_to_guess]
-    pack.fonctions.display_word(gamer_answer)
+    gamer_answer = ["*"] * len(word_to_guess)
+    print(''.join(gamer_answer))
 
     while attempt < pack.donnees.LIFE:
         l = pack.fonctions.gamer_letter()
         for i in range(len(word_to_guess)):
             if word_to_guess[i] == l:
                 gamer_answer[i] = l
-        pack.fonctions.display_word(gamer_answer)
+        print(''.join(gamer_answer))
 
         if "*" not in gamer_answer:
             print(f"Vous avez gagne en {attempt + 1} coups. " +
@@ -37,11 +37,11 @@ with open("package/scores", "wb") as scores_file:
             print(f"Il vous reste {pack.donnees.LIFE - attempt- 1} coups")
             attempt += 1
         else:
-            print(f"Vous avez perdu. La reponse est {pack.fonctions.display_word(word_to_guess)}")
+            print(f"Vous avez perdu. La reponse est : {''.join(word_to_guess)}")
             if pack.fonctions.replay() == 'o':
                 word_to_guess = list(random.choice(pack.donnees.WORDS))
                 gamer_answer = ["*" for x in word_to_guess]
-                pack.fonctions.display_word(gamer_answer)
+                print(''.join(gamer_answer))
                 attempt = 0
             else:
                 attempt += 1
