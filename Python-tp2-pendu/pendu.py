c2829db7c6d8436693c7cf4ef94e99e230a4a15d
j-bd <recodata.out@gmail.com>
2018-12-05 16:41:13 +0100

    Improvment of replay and initializing

--- Python-tp2-pendu/pendu.py
+++ Python-tp2-pendu/pendu.py
@@ -4,24 +4,23 @@
 """Pendu Game"""
 
 import pickle
-import random
 
 import pack.donnees
 import pack.fonctions
 
 
 print("Bienvenue dans le jeu du Pendu")
-scores_dict = pack.fonctions.scores_checking()
-player, score, scores_dict = pack.fonctions.gamer_history(scores_dict)
-attempt = 0
+SCORES_DICT = pack.fonctions.scores_checking()
+PLAYER, SCORE, SCORES_DICT = pack.fonctions.gamer_history(SCORES_DICT)
+PLAY = bool(1)
+ENDED = bool(1)
 
 with open("pack/scores", "wb") as scores_file:
 
-    word_to_guess = list(random.choice(pack.donnees.WORDS))
-    gamer_answer = ["*"] * len(word_to_guess)
-    print(''.join(gamer_answer))
+    while PLAY:
+        if ENDED:
+            word_to_guess, gamer_answer, attempt, ENDED = pack.fonctions.initiate()
 
-    while attempt < pack.donnees.LIFE:
         l = pack.fonctions.gamer_letter()
         for i in range(len(word_to_guess)):
             if word_to_guess[i] == l:
@@ -30,20 +29,14 @@ with open("pack/scores", "wb") as scores_file:
 
         if "*" not in gamer_answer:
             print(f"Vous avez gagne en {attempt + 1} coups. " +
-                  f"Votre score est de {score + pack.donnees.LIFE - attempt}")
-            scores_dict[player] = score + pack.donnees.LIFE - attempt
-            break
+                  f"Votre score est de {SCORE + pack.donnees.LIFE - attempt}")
+            SCORES_DICT[PLAYER] = SCORE + pack.donnees.LIFE - attempt
+            PLAY, ENDED = pack.fonctions.replay()
         elif attempt + 1 < pack.donnees.LIFE:
             print(f"Il vous reste {pack.donnees.LIFE - attempt- 1} coups")
             attempt += 1
         else:
             print(f"Vous avez perdu. La reponse est : {''.join(word_to_guess)}")
-            if pack.fonctions.replay() == 'o':
-                word_to_guess = list(random.choice(pack.donnees.WORDS))
-                gamer_answer = ["*" for x in word_to_guess]
-                print(''.join(gamer_answer))
-                attempt = 0
-            else:
-                attempt += 1
+            PLAY, ENDED = pack.fonctions.replay()
 
-    pickle.dump(scores_dict, scores_file)
+    pickle.dump(SCORES_DICT, scores_file)
