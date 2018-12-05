0827f5e54077bbb86bc5e14d3e348cce4c3e39d5
j-bd <recodata.out@gmail.com>
2018-12-05 16:38:56 +0100

    New Functions

--- Python-tp2-pendu/pack/fonctions.py
+++ Python-tp2-pendu/pack/fonctions.py
@@ -4,6 +4,9 @@
 """Pendu Game functions"""
 
 import pickle
+import random
+
+import pack.donnees
 
 
 def scores_checking():
@@ -25,26 +28,34 @@ def scores_checking():
 
 def gamer_history(scores_dict):
     """Checking if the gamer have already a score and create one if not"""
-    print("Saisissez votre nom de joureur : ")
-    answer = input()
-    if answer in scores_dict.keys():
-        return answer, scores_dict.get(answer), scores_dict
-    else:
-        return answer, 0, scores_dict
+    answer = input("Saisissez votre nom de joureur : ")
+    return answer, scores_dict.get(answer, 0), scores_dict
 
 
-def gamer_letter(letter="0"):
+def gamer_letter():
     """Asking  the gamer to choose a letter and return it"""
-    while letter.isalpha() != True or len(letter) > 1:
-        print("S'il vous plait, choisissez votre lettre: ")
-        letter = input()
+    letter = input("S'il vous plait, choisissez votre lettre: ")
+    while letter.isalpha() is False or len(letter) > 1:
+        letter = input("Veuillez ressaisir votre lettre: ")
     return letter.lower()
 
 
-def replay(answer="0"):
+def replay():
     """Offer the possibility to play again"""
+    answer = input("Souhaitez-vous rejouer (o/n) ?")
     while answer.lower() != "o" and answer.lower() != "n":
-        print("Souhaitez-vous rejouer (o/n) ?")
-        answer = input()
-    return answer
-    
+        answer = input("Veuillez ressaisir votre choix. Souhaitez-vous rejouer (o/n) ?")
+
+    if answer == 'o':
+        play = bool(1)
+    else:
+        play = bool(0)
+    return play, bool(1)
+
+
+def initiate():
+    """Return the word to guess by the player"""
+    word_to_guess = list(random.choice(pack.donnees.WORDS))
+    gamer_answer = ["*" for x in word_to_guess]
+    print(''.join(gamer_answer))
+    return word_to_guess, gamer_answer, 0, bool(0)
