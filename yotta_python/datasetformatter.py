18e8878b2e9b091f7e998935bc35a9f721b51d86
j-bd <recodata.out@gmail.com>
2020-03-24 18:09:14 +0100

    private method and property

--- yotta_python/datasetformatter.py
+++ yotta_python/datasetformatter.py
@@ -25,7 +25,7 @@ class DatasetFormatter:
         self.check_extension()
         self.df = self.read_data()
 
-    def check_extension(self):
+    def _check_extension(self):
         """Check extension of input file"""
         self.extension = c.INPUT_FILE_NAME.split(sep=".")[-1].lower()
         if self.extension not in ["csv", "parquet"]:
@@ -34,7 +34,7 @@ class DatasetFormatter:
                 " file"
             )
 
-    def read_data(self):
+    def _read_data(self):
         """Return a dataframe"""
         if self.extension == "csv":
             df = pd.read_csv(c.INPUT_FILE_NAME)
@@ -42,11 +42,11 @@ class DatasetFormatter:
             df = pd.read_parquet(c.INPUT_FILE_NAME, engine='pyarrow')
         return df
 
-    def rename_columns(self):
+    def _rename_columns(self):
         """Rename columns following a pre-defined pattern in constants file"""
         self.df.rename(columns=c.COLUMNS_NAMES_VARIATION, inplace=True)
 
-    def format_date(self):
+    def _format_date(self):
         """Provide the right date format"""
         for line, val in enumerate(self.df.loc[:, c.COL_KEY["date"]]):
             date_format = datetime.datetime.strptime(
@@ -54,7 +54,7 @@ class DatasetFormatter:
             ).date()
             self.df.loc[line, c.COL_KEY["date"]] = date_format
 
-    def clean_letters(self):
+    def _clean_letters(self):
         """Remove accent letters"""
         for col_name in c.COL_STR_FORMAT:
             for line, val in enumerate(self.df.loc[:, col_name]):
@@ -62,15 +62,16 @@ class DatasetFormatter:
                     .encode('ascii', 'ignore').decode()
                 self.df.loc[line, col_name] = word
 
-    def rename_cities(self):
+    def _rename_cities(self):
         """Rename cities"""
         self.df.replace(c.CITIES_NAMES_VARIATION, inplace=True)
 
-    def check_columns_format(self):
+    def _check_columns_format(self):
         """Check if columns numbers are set to float"""
         for col_name in c.COL_NUMBER_FORMAT:
             self.df[col_name] = pd.to_numeric(self.df[col_name])
 
+    @property
     def process_pipeline(self):
         """Launch full processing pipeline"""
         self.rename_columns()
