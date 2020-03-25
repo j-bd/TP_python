bad739262d75f4527c4477498133e0a6845b43c9
j-bd <recodata.out@gmail.com>
2020-03-25 21:01:43 +0100

    packaging

---
+++ yotta_python/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 20:56:01 2020
+
+@author: j-bd
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='sales_processing',
+      author="j-bd",
+      packages=find_packages()
+)
