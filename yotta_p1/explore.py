ef6855494bc894b4ecb95149243a511a257a1ed6
j-bd <recodata.out@gmail.com>
2020-04-13 21:25:42 +0200

    full pipeline

--- yotta_p1/explore.py
+++ yotta_p1/explore.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  6 14:30:05 2020
+Created on Mon Apr 13 14:03:02 2020
 
 @author: j-bd
 """
@@ -12,14 +12,19 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import f1_score
 from sklearn.metrics import auc
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
 
 
 
@@ -47,10 +52,10 @@ print(df_data.nunique(dropna = False))
 # Clean data
 # =============================================================================
 
-df_data.drop(columns=["DATE", "DURATION_CONTACT", "RESULT_LAST_CAMPAIGN"], inplace=True)
+df_data.drop(columns=["DATE", "DURATION_CONTACT", "RESULT_LAST_CAMPAIGN", "CONTACT"], inplace=True)
 
-numeric_variables = df_data.select_dtypes(include=[np.number])
-cat_variables = df_data.select_dtypes(include=[object])
+numeric_variables = list(df_data.select_dtypes(include=['int64', 'float64']))
+cat_variables = list(df_data.select_dtypes(include=[object]).drop(['SUBSCRIPTION'], axis=1))
 
 # =============================================================================
 # Visualisation
@@ -61,7 +66,7 @@ numeric_variables_variance = numeric_variables.var()
 print(numeric_variables_variance)
 
 # univariee quantitative
-for col in numeric_variables.columns:
+for col in numeric_variables:
     plt.figure()
     graph = sns.countplot(x=col, hue='SUBSCRIPTION', data=df_data)
     graph.set_yscale("log")
@@ -73,15 +78,15 @@ df_data.plotting.scatter_matrix() # method : {‘pearson’, ‘kendall’, ‘s
 
 
 #univariee qualitative
-for col in cat_variables.columns:
+for col in cat_variables:
     plt.figure()
-    graph = sns.countplot(x=col, hue='SUBSCRIPTION', data=cat_variables)
+    graph = sns.countplot(x=col, hue='SUBSCRIPTION', data=df_data)
     graph.set_yscale("log")
 
 
 # multivarie qualitative avec qualitative
-for cat in cat_variables.columns:
-    for num in numeric_variables.columns:
+for cat in cat_variables:
+    for num in numeric_variables:
         plt.figure()
         sns.boxplot(x=num, y=cat, data=df_data)
 
@@ -91,20 +96,15 @@ sns.heatmap(df_data.corr(), nominal_columns=cat_variables.columns, annot=True)
 
 
 # =============================================================================
-# Valeurs manquantes et aberrantes
+# Valeurs aberrantes
 # =============================================================================
-print(df_data.isnull().sum(axis=0) *100 / len(df_data))
-for col in df_data.columns:
-    df_data[col].fillna(df_data[col].mode()[0],inplace = True)
-print(df_data.isnull().sum(axis=0) *100 / len(df_data))
-
 df_data.shape
 for col in df_data.select_dtypes(include=[np.number]).columns:
     df_data = df_data[
             np.abs(df_data[col] - df_data[col].mean()) <= (3 * df_data[col].std())
     ]
 df_data.shape
-
+print(df_data.info())
 
 # =============================================================================
 # Train test split
@@ -118,36 +118,62 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 
 # =============================================================================
-# Encodage
+# Transformation des donnees
 # =============================================================================
+print(x_train.isnull().sum(axis=0) *100 / len(df_data))
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', MinMaxScaler())])
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-x_train = pd.get_dummies(x_train, drop_first=True)
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_variables),
+        ('cat', categorical_transformer, cat_variables)
+    ]
+)
+
+
+
+# =============================================================================
+#Pipeline and grid search
+# =============================================================================
+pca = PCA(whiten=True, random_state=42)
+svc = SVC(kernel='rbf', class_weight='balanced')
+#model = make_pipeline(preprocessor, pca, svc)
+model = Pipeline(steps=[('preprocessor', preprocessor),
+                        ('pca', pca),
+                      ('svc', svc)])
+
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+param_grid = {
+    'pca__n_components': [5, 13],
+    'svc__C': [1, ],
+    'svc__gamma': [0.0001, 0.001]}
+grid = GridSearchCV(model, param_grid, n_jobs=-1, cv=skf)
+
 y_train = pd.get_dummies(y_train, drop_first=True)
 
-# =============================================================================
-# Mise a l'echelle
-# =============================================================================
-scaler = MinMaxScaler()
-x_train = scaler.fit_transform(x_train)
+%time grid.fit(x_train, y_train)
+print(grid.get_params)
+print(grid.best_params_)
 
-
-# =============================================================================
-# Entrainement
-# =============================================================================
-classifier = LogisticRegression()
-classifier_trained = classifier.fit(x_train, y_train)
-
+model = grid.best_estimator_
+y_pred = model.predict(x_test)
 
 # =============================================================================
 # Prediction
 # =============================================================================
 
-x_test = pd.get_dummies(x_test, drop_first=True)
+#x_test = pd.get_dummies(x_test, drop_first=True)
 y_test = pd.get_dummies(y_test, drop_first=True)
+#
+#x_test = scaler.fit_transform(x_test)
 
-x_test = scaler.fit_transform(x_test)
-
-y_pred = classifier_trained.predict(x_test)
+#y_pred = classifier_trained.predict(x_test)
 
 
 # =============================================================================
@@ -175,8 +201,12 @@ plt.show()
 
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm,annot=True)
-classifier_trained.score(x_test, y_test)
+#classifier_trained.score(x_test, y_test)
+#
+#ys_rate = pd.get_dummies(ys, drop_first=True)
+#print(f"Taux d'acceptation sur l'ensemble du dataset: {1 - ys_rate.sum() / len(df_data)}"\
+#    f"Taux predit : {classifier_trained.score(x_test, y_test)}")
 
 ys_rate = pd.get_dummies(ys, drop_first=True)
 print(f"Taux d'acceptation sur l'ensemble du dataset: {1 - ys_rate.sum() / len(df_data)}"\
-    f"Taux predit : {classifier_trained.score(x_test, y_test)}")
+    f"Taux predit : {model.score(x_test, y_test)}")
