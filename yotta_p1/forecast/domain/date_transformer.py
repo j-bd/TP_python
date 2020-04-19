0adb7bb002f88e1c9d716186b3fbdad59eff16b9
j-bd <recodata.out@gmail.com>
2020-04-19 10:28:48 +0200

    init suppression

--- yotta_p1/forecast/domain/date_transformer.py
+++ yotta_p1/forecast/domain/date_transformer.py
@@ -8,15 +8,12 @@ Classes
 DateTransformer
 
 """
-from dataclasses import dataclass, field
-
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
-import forecast.settings.base as stg
+import forecast.settings as stg
 
 
-@dataclass
 class DateTransformer(BaseEstimator, TransformerMixin):
     """
     Process date feature.
@@ -29,16 +26,7 @@ class DateTransformer(BaseEstimator, TransformerMixin):
 
     """
 
-    input_df: pd.DataFrame
-    X: pd.DataFrame = field(init=False)
-    y: pd.DataFrame = field(init=False)
-
-
-    def __post_init__(self):
-        self.X = self.input_df.drop(columns=[stg.DATA_SUBSCRIPTION])
-        self.y = self.input_df[stg.DATA_SUBSCRIPTION]
-
-    def fit(self):
+    def fit(self, X, y=None):
         """Fit method that return the object itself.
 
         Parameters
@@ -54,7 +42,7 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         """
         return self
 
-    def transform(self):
+    def transform(self, X, y=None):
         """Transform method that return transformed DataFrame.
 
         Parameters
@@ -68,31 +56,30 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         -------
         X: pandas.DataFrame
         """
-        if self.X[stg.DATA_DATE].isnull().any():
-            self.X = self.fill_missing_value(self.X)
+        cls = self.__class__
+        if X[stg.DATA_DATE].isnull().any():
+            X = cls.fill_missing_value(X)
 
-        self.X['DATE'] = pd.to_datetime(self.X['DATE'], format=stg.DATA_DATE_FORMAT)
-        self.X["day"] = self.X['DATE'].dt.day #remove ?
-        self.X["year"] = self.X['DATE'].dt.year #remove ?
-        self.X["weekday"] = self.X['DATE'].dt.day_name()
-        self.X["month"] = self.X['DATE'].dt.month_name()
+        X['DATE'] = pd.to_datetime(X['DATE'], format=stg.DATA_DATE_FORMAT)
+        X["weekday"] = X['DATE'].dt.day_name()
+        X["month"] = X['DATE'].dt.month_name()
 
-        self.X["day_selected"] = self.X["weekday"].apply(
+        X["day_selected"] = X["weekday"].apply(
             lambda x: 0 if x in stg.WEEKEND else 1
         )
-        self.X["hot_month"] = self.X["month"].apply(
+        X["hot_month"] = X["month"].apply(
             lambda x: 1 if x in stg.HOT_MONTH else 0
         )
-        self.X["warm_month"] = self.X["month"].apply(
+        X["warm_month"] = X["month"].apply(
             lambda x: 1 if x in stg.WARM_MONTH else 0
         )
-        self.X["cold_month"] = self.X["month"].apply(
+        X["cold_month"] = X["month"].apply(
             lambda x: 1 if x in stg.COLD_MONTH else 0
         )
         # Return only features columns
-        return self.X.filter(items=stg.DATE_COLS)
+        return X.filter(items=stg.DATE_COLS)
 
-    def fill_missing_value(self, df):
+    def fill_missing_value(X):
         """Transform method that return transformed DataFrame.
 
         Parameters
@@ -106,13 +93,12 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         -------
         X: pandas.DataFrame
         """
-        return df[stg.DATA_DATE].fillna(method='ffill', inplace=True)
+        return X[stg.DATA_DATE].fillna(method='ffill', inplace=True)
 
 
 if __name__ == "__main__":
     merged_input = "data/interim/data_socio_merged.csv"
     data_output = "data/interim/data_date.csv"
     input_df = pd.read_csv(merged_input)
-    date_df = DateTransformer(input_df)
-    date_df = date_df.transform()
+    date_df = DateTransformer().fit_transform(input_df)
     date_df.to_csv(data_output, index=False)
