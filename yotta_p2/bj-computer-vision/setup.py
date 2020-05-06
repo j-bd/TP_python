2d6f7e59dbab8f7d572a1d518ebade4be012dc43
j-bd <recodata.out@gmail.com>
2020-05-07 00:12:08 +0200

    named change

--- yotta_p2/bj-computer-vision/setup.py
+++ yotta_p2/bj-computer-vision/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='face_detection',
+    name='masked_face',
     packages=find_packages(),
     version='1.0.0',
     description='Code to detect masked people.',
