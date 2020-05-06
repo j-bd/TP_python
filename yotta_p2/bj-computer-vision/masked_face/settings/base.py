ff3c92a304932946c977494f1d371bc0e96b6545
j-bd <recodata.out@gmail.com>
2020-05-07 00:17:34 +0200

    default path

--- yotta_p2/bj-computer-vision/masked_face/settings/base.py
+++ yotta_p2/bj-computer-vision/masked_face/settings/base.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May  3 12:55:52 2020
-
-@author: j-bd
-"""
 
 # Path variables
+DATA_FILE = '/home/latitude/Documents/Yotta/2-Data_Science/Projet_2-CV_NLP/data/temp_data'
+MODEL_FILE = '/home/latitude/Documents/Yotta/yotta_exs/yotta_p2/bj-computer-vision/models'
 
 # Image constants
-WIDTH = 28
+IMAGE_SIZE = {'MobileNetV2': (224, 224, 3)}
+
+# Model constants
+MODEL_CHOICE = 'MobileNetV2'
+CLASS_NBR = 2
+BATCH_SIZE = 32
+INIT_LEARNING_RATE = 1e-4
+EPOCHS = 2
+
+# Developper mode
