7e1fa26ee648b86feda8911e96175c44f5bc4bf3
j-bd <recodata.out@gmail.com>
2020-05-07 00:10:31 +0200

    conda env

--- yotta_p2/bj-computer-vision/activate.sh
+++ yotta_p2/bj-computer-vision/activate.sh
@@ -1,19 +1,20 @@
 function activate_venv() {
-    # try to activate a "regular .venv"
-    . ./.venv/bin/activate || \
-        # if it fails, try to activate with conda
-        conda activate ./.venv || \
+    # try to activate with conda
+    conda activate yp2 || \
+        # if it fails, try to activate a "regular .venv"
+        #./.venv/bin/activate || \
         # if it fails: warn the user and exit
-        ( echo ""; echo "ERROR: failed to activate virtual environment .venv! ask for advice on #dev "; return 1 )
+        ( echo ""; echo "ERROR: failed to activate virtual environment yp2! ask for advice on #dev "; return 1 )
 }
 
 activate_venv && (
     echo ""
     echo "************************************************************************************"
     echo "Successfuly activated the virtual environment; you are now using this python:"
-    echo "$ which python"
-    echo "$(which python)"
+    echo "$ which python3"
+    echo "$(which python3)"
     echo "************************************************************************************"
     echo ""
 )
+
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
