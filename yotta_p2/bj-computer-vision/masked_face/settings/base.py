0457fce3e7fa882c751d56e616f5715be2d925f7
j-bd <recodata.out@gmail.com>
2020-05-10 15:49:29 +0200

    webcam constants renamed

--- yotta_p2/bj-computer-vision/masked_face/settings/base.py
+++ yotta_p2/bj-computer-vision/masked_face/settings/base.py
@@ -3,13 +3,16 @@
 import os
 
 # Path variables
+# --------------
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 RAW_DIR = os.path.join(REPO_DIR, 'data', 'raw')
 LOGS_DIR = os.path.join(REPO_DIR, 'logs')
 MODELS_DIR = os.path.join(REPO_DIR, 'models')
 OUTPUT_DIR = os.path.join(REPO_DIR, 'data', 'processed')
-WEBCAM_DETECTION_DIR = os.path.join(MODELS_DIR, 'webc_face_detector')
 
+
+# Train variables
+# ---------------
 # Image constants
 IMAGE_SIZE = {'MobileNetV2': (224, 224, 3)}
 
@@ -22,10 +25,34 @@ EPOCHS = 20
 MODEL_FILE = os.path.join(MODELS_DIR, MODEL_CHOICE + '-masked_detection.hdf5')
 LABELS_NAME = ['masked', 'no_masked']
 
+
+# Predict variables
+# -----------------
 # Webcam Face detector
 WEBC_MODEL_CLASSIFIER = 'MobileNetV2'
-WEBC_MODEL_DETECTION_STRUCTURE = os.path.join(WEBCAM_DETECTION_DIR, 'deploy.prototxt')
-WEBC_MODEL_DETECTION_WEIGHT = os.path.join(WEBCAM_DETECTION_DIR, 'res10_300x300_ssd_iter_140000.caffemodel')
+WEBC_MODEL_CLASSIFIER_FILE = os.path.join(
+    MODELS_DIR, WEBC_MODEL_CLASSIFIER + '-masked_detection.hdf5'
+)
+WEBC_MODEL_DETECTION_STRUCTURE = os.path.join(
+    MODELS_DIR, 'res10_caffe', 'deploy.prototxt'
+)
+WEBC_MODEL_DETECTION_WEIGHT = os.path.join(
+    MODELS_DIR, 'res10_caffe', 'res10_300x300_ssd_iter_140000.caffemodel'
+)
+
+# Video Face detector
+VIDEO_MODEL_CLASSIFIER = 'MobileNetV2'
+VIDEO_MODEL_CLASSIFIER_FILE = os.path.join(
+    MODELS_DIR, VIDEO_MODEL_CLASSIFIER + '-masked_detection.hdf5'
+)
+VIDEO_MODEL_DETECTION_STRUCTURE = os.path.join(
+    MODELS_DIR, 'res10_caffe', 'deploy.prototxt'
+)
+VIDEO_MODEL_DETECTION_WEIGHT = os.path.join(
+    MODELS_DIR, 'res10_caffe', 'res10_300x300_ssd_iter_140000.caffemodel'
+)
+
 
 # Developper mode
+# ---------------
 DATA_FILE = '/home/latitude/Documents/Yotta/2-Data_Science/Projet_2-CV_NLP/data/final_data'  # TODO removed at the end of development
