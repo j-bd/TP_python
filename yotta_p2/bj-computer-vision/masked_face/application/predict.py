77c57f4d0b3805779adbd23b217a64938d8ddc23
j-bd <recodata.out@gmail.com>
2020-05-11 09:36:25 +0200

    image call

--- yotta_p2/bj-computer-vision/masked_face/application/predict.py
+++ yotta_p2/bj-computer-vision/masked_face/application/predict.py
@@ -18,7 +18,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 def main():
-    """Launch the main process of algorithm training"""
+    """Launch the main process of predict training"""
     # Command line parser
     parser = PredictCommandLineParser()
     args = parser.parse_args()
@@ -33,9 +33,28 @@ def main():
         logging.info(' To stop processing please press the letter "q"')
         webcam_pipe = Pipeline(detector, classifier, args)
         webcam_pipe.webcam_detection()
-    elif args['type_detection'] == 'video':
 
-        logging.info(' Detection ended')
+    elif args['type_detection'] == 'video':
+        logging.info(' Loading models ...')
+        models = GetModels(args['type_detection'])
+        detector, classifier = models.models_loading()
+
+        logging.info(' Starting video analyse ...')
+        logging.info(' To stop processing please press the letter "q"')
+        video_pipe = Pipeline(detector, classifier, args)
+        video_pipe.video_detection()
+
+    elif args['type_detection'] == 'image':
+        logging.info(' Loading models ...')
+        models = GetModels(args['type_detection'])
+        detector, classifier = models.models_loading()
+
+        logging.info(' Starting video analyse ...')
+        logging.info(' To stop processing please press the letter "q"')
+        image_pipe = Pipeline(detector, classifier, args)
+        image_pipe.image_detection()
+
+    logging.info(' Detection ended')
 
 
 if __name__ == "__main__":
