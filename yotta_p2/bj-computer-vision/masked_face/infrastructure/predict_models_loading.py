edfb608f2517ef23f3c2cf75eda107345a1e7d84
j-bd <recodata.out@gmail.com>
2020-05-11 09:47:23 +0200

    model loading improvment and docstring

--- yotta_p2/bj-computer-vision/masked_face/infrastructure/predict_models_loading.py
+++ yotta_p2/bj-computer-vision/masked_face/infrastructure/predict_models_loading.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Module to retrieve models.
 
+Classes
+-------
+GetModels
 """
 import cv2
 from tensorflow.keras.models import load_model
@@ -10,11 +14,18 @@ from masked_face.settings import base
 
 
 class GetModels:
+    """Set up models.
+
+    Methods
+    -------
+    models_loading
+    """
     def __init__(self, type_detection):
         """Class initialisation
+
         Parameters
         ----------
-        type_detection : str.
+        type_detection : str
             user specification
         """
         self.type_input = type_detection
@@ -39,12 +50,23 @@ class GetModels:
             )
             classifier = load_model(model_classification)
             return detector, classifier
+
         elif self.type_input == 'video':
-#            model_classification = base.MODEL_FILE
-#            model_detection_structure = base.WEBC_MODEL_DETECTION_STRUCTURE
-#            model_detection_weights = base.WEBC_MODEL_DETECTION_WEIGHT
-#            detector = cv2.dnn.readNet(
-#                model_detection_structure, model_detection_weights
-#            )
-#            classifier = load_model(model_classification)
+            model_classification = base.VIDEO_MODEL_CLASSIFIER_FILE
+            model_detection_structure = base.VIDEO_MODEL_DETECTION_STRUCTURE
+            model_detection_weights = base.VIDEO_MODEL_DETECTION_WEIGHT
+            detector = cv2.dnn.readNet(
+                model_detection_structure, model_detection_weights
+            )
+            classifier = load_model(model_classification)
+            return detector, classifier
+
+        elif self.type_input == 'image':
+            model_classification = base.IMAGE_MODEL_CLASSIFIER_FILE
+            model_detection_structure = base.IMAGE_MODEL_DETECTION_STRUCTURE
+            model_detection_weights = base.IMAGE_MODEL_DETECTION_WEIGHT
+            detector = cv2.dnn.readNet(
+                model_detection_structure, model_detection_weights
+            )
+            classifier = load_model(model_classification)
             return detector, classifier
