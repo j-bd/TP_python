70a5c0e0c641f7bfec27a07f799baa92c1a42b6b
j-bd <recodata.out@gmail.com>
2020-05-17 15:22:42 +0200

    big re organisation of main predict

--- yotta_p2/bj-computer-vision/masked_face/application/predict.py
+++ yotta_p2/bj-computer-vision/masked_face/application/predict.py
@@ -1,61 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Module to .
+Module to make predictions.
 
-Classes
+Example
 -------
+Script could be run with the following command line
 
+    $ python masked_face/application/predict.py ----type_detection 'video'
+    --path_video 'path/to/video.mp4' --classifier_type 'MobileNetV2'
+
+    $ python masked_face/application/predict.py ----type_detection 'image'
+    --path_image 'path/to/image.jpg' --classifier_type 'MobileNetV2'
+
+--type_detection: 3 types of detection are offered 'image', 'video', 'webcam'.
+
+--path_video: full path to your video
+
+--classifier_type: 3 types of classifiers are offered 'MobileNetV2', 'VGG16',
+'Xception'.
 """
 import logging
 
-from masked_face.domain.pipeline_detection import Pipeline
+from masked_face.domain.predict_medium import MediumSelection
 from masked_face.infrastructure.predict_models_loading import GetModels
-from masked_face.infrastructure.command_line_parser import PredictCommandLineParser
+from masked_face.infrastructure.command_line_parser import PredictParser
 
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-def main():
+def main(args):
     """Launch the main process of predict training"""
-    # Command line parser
-    parser = PredictCommandLineParser()
-    args = parser.parse_args()
+    # Loading models
+    logging.info(' Loading models ...')
+    models = GetModels(args['classifier_type'])
+    detector, classifier = models.models_loading()
 
-    # Detection type choice
-    if args['type_detection'] == 'webcam':
-        logging.info(' Loading models ...')
-        models = GetModels(args['type_detection'])
-        detector, classifier = models.models_loading()
-
-        logging.info(' Starting webcam analyse ...')
-        logging.info(' To stop processing please press the letter "q"')
-        webcam_pipe = Pipeline(detector, classifier, args)
-        webcam_pipe.webcam_detection()
-
-    elif args['type_detection'] == 'video':
-        logging.info(' Loading models ...')
-        models = GetModels(args['type_detection'])
-        detector, classifier = models.models_loading()
-
-        logging.info(' Starting video analyse ...')
-        logging.info(' To stop processing please press the letter "q"')
-        video_pipe = Pipeline(detector, classifier, args)
-        video_pipe.video_detection()
-
-    elif args['type_detection'] == 'image':
-        logging.info(' Loading models ...')
-        models = GetModels(args['type_detection'])
-        detector, classifier = models.models_loading()
-
-        logging.info(' Starting video analyse ...')
-        logging.info(' To stop processing please press the letter "q"')
-        image_pipe = Pipeline(detector, classifier, args)
-        image_pipe.image_detection()
+    # Launching detection
+    processing_medium = MediumSelection(detector, classifier, args)
+    processing_medium.medium_pipeline_selection()
 
     logging.info(' Detection ended')
 
 
 if __name__ == "__main__":
-    main()
+    # Command line parser
+    parser = PredictParser()
+    args = parser.parse_args()
+    main(args)
