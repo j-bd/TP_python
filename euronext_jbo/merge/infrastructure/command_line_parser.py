88042e002ce4498faadf52146c74776ecdd90bbf
j-bd <recodata.out@gmail.com>
2020-07-29 08:47:16 +0200

    parse files path

--- euronext_jbo/merge/infrastructure/command_line_parser.py
+++ euronext_jbo/merge/infrastructure/command_line_parser.py
@@ -5,17 +5,16 @@
 
 Classes
 -------
-TrainCommandLineParser
-PredictCommandLineParser
+MergeCommandLineParser
 
 """
 
 from argparse import ArgumentParser
 
-import forecast.settings as stg
+from merge.settings import base
 
 
-class TrainCommandLineParser():
+class MergeCommandLineParser:
     """Command line parser for train application.
 
     Methods
@@ -32,94 +31,20 @@ class TrainCommandLineParser():
     def _add_arguments(self):
         """Add arguments to the parser."""
         self.parser.add_argument(
-            "-d",
-            "--data_input",
-            help="path to input data file",
-            default=stg.DATA_FILE,
+            "-u", "--universe_input", help="path to universe file",
+            default=base.UNIVERSE_FILE,
             )
         self.parser.add_argument(
-            "-s",
-            "--socio_eco_input",
-            help="path to input socio eco file",
-            default=stg.SOCIO_ECO_FILE,
+            "-v", "--vigeo_input", help="path to vigeo file",
+            default=base.VIGEO_FILE,
             )
         self.parser.add_argument(
-            "-m",
-            "--model_output",
-            help="path to model file output",
-            default=stg.MODEL_FILE,
-            )
-        self.parser.add_argument(
-            "-t",
-            "--threshold",
-            help="threshold of the prediction",
-            default=.5,
-            type=float
-            )
-        self.parser.add_argument(
-            "--devmode",
-            help="developper mode",
-            action="store_true"
-            )
-        self.parser.add_argument(
-            "--optimisation",
-            help="Bayesian optimisation process activation",
-            default=False,
+            "-f", "--filter_input", help="path to filter file",
+            default=base.ISIN_EID_FILTER_FILE,
             )
 
     def parse_args(self):
-        """Parse train command line arguments.
-
-        Returns
-        -------
-        self.parser.parse_args(): Namespace
-        """
-        return self.parser.parse_args()
-
-
-class PredictCommandLineParser():
-    """Command line parser for predict application.
-
-    Methods
-    -------
-    parse_args
-
-    """
-
-    def __init__(self):
-        """Initialize class."""
-        self.parser = ArgumentParser()
-        self._add_arguments()
-
-    def _add_arguments(self):
-        """Add arguments to the parser."""
-        self.parser.add_argument(
-            "-d",
-            "--data_input",
-            help="path to input data file",
-            default=stg.DATA_WITHOUT_TARGET_FILE,
-            )
-        self.parser.add_argument(
-            "-s",
-            "--socio_eco_input",
-            help="path to input socio eco file",
-            default=stg.SOCIO_ECO_FILE,
-            )
-        self.parser.add_argument(
-            "-m",
-            "--model_input",
-            help="path to input model file",
-            default=stg.MODEL_FILE,
-            )
-        self.parser.add_argument(
-            "-p",
-            "--predict_output",
-            help="path to predict file output",
-            default=stg.PREDICT_FILE,
-            )
-
-    def parse_args(self):
-        """Parse predict command line arguments.
+        """Parse main command line arguments.
 
         Returns
         -------
