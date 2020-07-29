d5bdbed19e581adb52bb3a53926d1eb125217cd6
j-bd <recodata.out@gmail.com>
2020-07-29 18:26:02 +0200

    universe columns dtype

--- euronext_jbo/merge/settings/base.py
+++ euronext_jbo/merge/settings/base.py
@@ -17,10 +17,31 @@ VIGEO_FILE = os.path.join(RAW_DATA_DIR, 'Vigeo_keys.xlsx')
 ISIN_EID_FILTER_FILE = os.path.join(RAW_DATA_DIR, 'F7_ISIN_to_EID_Filter.csv')
 UNIVERSE_VIGEO_FILE = os.path.join(PROCESSED_DATA_DIR, 'universe_vigeo.csv')
 
+# Universe file
+U_HISTORICAL_ISIN = 'Historical_ISIN'
+U_ISIN = 'ISIN'
+U_SHORT_NAME = 'Short Name'
+U_CUTOFF = 'cutoff'
+U_FACTSET_ENTITY_ID = 'factset_entity_id'
+U_COLUMNS = [
+    U_HISTORICAL_ISIN, U_ISIN, U_SHORT_NAME, U_CUTOFF, U_FACTSET_ENTITY_ID
+]
+U_COLUMNS_CAST = {
+    U_HISTORICAL_ISIN: 'category', U_ISIN: 'category', U_SHORT_NAME: 'object',
+    U_CUTOFF: 'datetime64', U_FACTSET_ENTITY_ID: 'category'
+}
 
+# Vigeo file
+V_VIGEO_KEY = 'Vigeo_Key'
+V_CUTOFF = 'Cutoff_dates'
+V_ISIN = 'ISIN'
+V_TITLE = 'Title'
+V_COLUMNS = [V_VIGEO_KEY, V_CUTOFF, V_ISIN, V_TITLE]
 
-
-
+# F7_ISIN_to_EID_Filter file
+F_ISIN = 'isin'
+F_FACTSET_ENTITY_ID = 'factset_entity_id'
+F_COLUMNS = [F_ISIN, F_FACTSET_ENTITY_ID]
 
 
 
