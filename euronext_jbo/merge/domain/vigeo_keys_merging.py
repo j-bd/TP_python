feaa2d677e626c566d281cdd905fdd3630bd0b4a
j-bd <recodata.out@gmail.com>
2020-07-31 16:04:32 +0200

    logging modification and conditions counter add

--- euronext_jbo/merge/domain/vigeo_keys_merging.py
+++ euronext_jbo/merge/domain/vigeo_keys_merging.py
@@ -52,15 +52,17 @@ class VigeoKeysMerging:
         universe_df: pandas.DataFrame
         """
         # log saving setup
-        log_saving.enable_logging(
-            log_filename=f'{basename(__file__)}.log',
-            logging_level=logging.DEBUG
-        )
+        # log_saving.enable_logging(
+        #     log_filename=f'{basename(__file__)}.log',
+        #     logging_level=logging.DEBUG
+        # )
         logging.info('----------------------------------')
         logging.info(f'Script {basename(__file__)}')
         logging.info('----------------------------------')
 
+        # local variable initialization
         counter = 0
+        c1, c2, c3, c4, c5, no_value = [0] * 6
         size = len(self.universe_df)
         for u_index, u_row in self.universe_df.iterrows():
             # Displays progress
@@ -70,26 +72,30 @@ class VigeoKeysMerging:
 
             first_condition = self.vigeo_df.loc[(self.vigeo_df[base.DATE] >= u_row[base.DATE]) & (self.vigeo_df[base.V_ISIN] == u_row[base.U_ISIN])].head(1)
             if len(first_condition) != 0:
-                logging.info(f" Condition number 1 valid. Value Added")
                 self.universe_df.loc[u_index, base.U_VIGEO_KEY] = first_condition[base.V_VIGEO_KEY].values[0]
+                logging.info(f" Condition number 1 valid. Value Added")
+                c1 += 1
                 continue
 
             second_condition = self.vigeo_df.loc[(self.vigeo_df[base.DATE] >= u_row[base.DATE]) & (self.vigeo_df[base.V_ISIN] == u_row[base.U_HISTORICAL_ISIN])].head(1)
             if len(second_condition) != 0:
-                logging.info(f" Condition number 2 valid. Value Added")
                 self.universe_df.loc[u_index, base.U_VIGEO_KEY] = second_condition[base.V_VIGEO_KEY].values[0]
+                logging.info(f" Condition number 2 valid. Value Added")
+                c2 += 1
                 continue
 
             third_condition = self.vigeo_df.loc[(self.vigeo_df[base.DATE] >= u_row[base.DATE]) & (self.vigeo_df[base.V_VIGEO_KEY] == u_row[base.U_ISIN])].head(1)
             if len(third_condition) != 0:
-                logging.info(f" Condition number 3 valid. Value Added")
                 self.universe_df.loc[u_index, base.U_VIGEO_KEY] = third_condition[base.V_VIGEO_KEY].values[0]
+                logging.info(f" Condition number 3 valid. Value Added")
+                c3 += 1
                 continue
 
             fourth_condition = self.vigeo_df.loc[(self.vigeo_df[base.DATE] >= u_row[base.DATE]) & (self.vigeo_df[base.V_VIGEO_KEY] == u_row[base.U_HISTORICAL_ISIN])].head(1)
             if len(fourth_condition) != 0:
-                logging.info(f" Condition number 4 valid. Value Added")
                 self.universe_df.loc[u_index, base.U_VIGEO_KEY] = fourth_condition[base.V_VIGEO_KEY].values[0]
+                logging.info(f" Condition number 4 valid. Value Added")
+                c4 += 1
                 continue
 
             fifth_condition = self.filter_df.loc[(self.filter_df[base.F_FACTSET_ENTITY_ID] == u_row[base.U_FACTSET_ENTITY_ID])]
@@ -98,9 +104,19 @@ class VigeoKeysMerging:
                 if len(fifth_condition) != 0:
                     self.universe_df.loc[u_index, base.U_VIGEO_KEY] = fifth_condition[base.V_VIGEO_KEY].values[0]
                     logging.info(f" Condition number 5 valid. Value Added")
+                    c5 += 1
                     continue
 
             logging.info(" No condition is matching. No value will be add")
+            no_value += 1
 
+        logging.info('----------------------------------')
+        logging.info('Results:')
+        logging.info(
+            f'Number of condition 1: {c1}, Number of condition 2: {c2},'
+            f'Number of condition 3: {c3}, Number of condition 4: {c4},'
+            f'Number of condition 5: {c5}, Number of empty values: {no_value}'
+        )
+        logging.info('----------------------------------')
         logging.info(f'End of script {basename(__file__)}')
         return self.universe_df
